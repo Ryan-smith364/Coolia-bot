67d3859f910bd4d6092e441265d8bebb233536d1
ryancharlessmith <ryan-smith@comcast.net>
2020-10-26 12:03:32 -0400

    added help, joke, advice commands

--- commands/joke.js
+++ commands/joke.js
@@ -6,14 +6,21 @@ module.exports = {
   execute( message, args){
 
 
-    fetch('https://sv443.net/jokeapi/v2/joke/Any')
+    fetch('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=racist,sexist,nsfw')
     .then(resp => resp.json())
-    .then(joke => { message.channel.send(joke.setup)
-      .then((msg)=> {
-        setTimeout(function(){
-          msg.edit(joke.setup + '\n' + joke.delivery);
-        }, 2000)
-      })
+    .then(joke => { 
+      console.log(joke)
+      if(joke.type === 'single'){
+        message.channel.send(joke.joke)
+      }
+      else{
+        message.channel.send(joke.setup)
+        .then((msg)=> {
+          setTimeout(function(){
+            msg.edit(joke.setup + '\n' + joke.delivery);
+          }, 2000)
+        })
+      }
     }) 
   }
 }
